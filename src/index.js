71811b4eaf9946d332627f414ee1cd5facc3766a
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-12-10 16:29:42 +0530

    feat: font scaling

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {
   turquoiseText, render
 } from './formatting'
 
-import {githubProjectOptions, terminalOptions} from './options';
+import { githubProjectOptions, terminalOptions } from './options';
 // figlet.defaults({ fontPath: 'https://unpkg.com/figlet@1.4.0/fonts/' });
 // figlet.preloadFonts(["Standard", "Slant"], ready);
 
@@ -16,11 +16,15 @@ async function loadFonts() {
 loadFonts();
 
 var term;
+var fontSize = 1.5;
 const commands = [
   { 'show-projects': 'populates a list of projects from Github' },
   { 'show-resume': 'shows resume in a new tab' },
   { 'show-social': 'follow me on social media' },
-  { 'alert': `opens a browser based alert . Usage: ${orangeText('alert')} ${orangeText('some text with or without spaces')}` }
+  { 'alert': `opens a browser based alert . Usage: ${orangeText('alert')} ${orangeText('some text with or without spaces')}` },
+  {'font-size-up': 'increases terminal font size by 0.2'},
+  {'font-size-down': 'decreases terminal font size by 0.2'},
+  
 ];
 
 var githubProjects = [];
@@ -36,11 +40,11 @@ const getGithubProjects = async () => {
     (data) => {
       data.map((repo, index) => {
         githubProjects.push({
-          'Sr.No': index + 1, 
+          'Sr.No': index + 1,
           name: repo.name,
           fork: repo.fork,
           description: repo.description,
-          lang: repo.language || 'other', 
+          lang: repo.language || 'other',
         });
       })
     }
@@ -65,6 +69,7 @@ const aboutMe = () => {
 const displayHelp = () => {
   return `${render('David Velho')}
     ${aboutMe()}
+    You can press [TAB] for auto complete, Ctrl+r for reverse search
     ${helpCommands()}`;
 }
 
@@ -75,6 +80,14 @@ function ready() {
     'show-projects': () => columnify(githubProjects, githubProjectOptions),
     echo: (...text) => text.join(' '),
     alert: (...text) => alert(text.join(' ')),
+    'font-size-up': () => {
+      fontSize += 0.2;
+      document.documentElement.style.cssText = `--size: ${fontSize}`;
+    },
+    'font-size-down': () => {
+      fontSize -= 0.2;
+      document.documentElement.style.cssText = `--size: ${fontSize}`;
+    }
 
   }, terminalOptions);
 }
