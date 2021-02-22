cd536437405bf8b8042517dc73fc5d2340714ed6
theProgrammerDavid <davidvelho2awesome@gmail.com>
2021-02-22 12:14:20 +0530

    refractor: new socials

--- src/constants.js
+++ src/constants.js
@@ -13,6 +13,9 @@ export const commands = [
     { 'me': `Displays my profile picture` },
 
 ];
+export const projects = [
+
+];
 export const achievements = [
     { 'No.': 1, title: 'LoR Dean SCOPE VIT, TVS Pilot Project', document_url: renderLink('https://raw.githubusercontent.com/theProgrammerDavid/portfolio/master/static/tvs_cert.jpeg', 'TVS') },
     { 'No.': 2, title: '2nd place Bolt Hack', document_url: renderLink('https://raw.githubusercontent.com/theProgrammerDavid/portfolio/master/static/bolt_hack.jpeg', 'Bolt Hack') },
@@ -24,7 +27,8 @@ export const achievements = [
 export const socials = [
     { 'No': 1, website: 'Github', url: 'https://github.com/theProgrammerDavid' },
     { 'No': 2, website: 'StackOverflow', url: 'https://stackoverflow.com/users/14312583/goanmafia' },
-
+    { 'No': 3, website: 'Artstation', url: 'https://www.artstation.com/david_velho' },
+    { 'No': 4, website: '500px', url: 'https://500px.com/p/david_velho?view=photos' },
 ]
 export const myPic = () => { return get_image('https://avatars1.githubusercontent.com/u/35698009?s=460&u=988e5ad85edb20cf16aaeeb8ea3e8b44088a582c&v=4') }
 
