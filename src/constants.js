f310da7440e933f834379605e16cf1a44299bad4
theProgrammerDavid <davidvelho2awesome@gmail.com>
2021-04-27 23:51:48 +0530

    refactor: updated socials

--- src/constants.js
+++ src/constants.js
@@ -44,7 +44,7 @@ export const terminalOptions = {
         return render('David Velho', 'Doom') +
             `\n${greenText(`Hey, I'm David`)}. Type in ${greenText(`man`)} / ${greenText('family-tech-support')} to get started.\n`;
     },
-    prompt: getPrompt() ,
+    prompt: getPrompt(),
     checkArity: false,
     history: true,
     exit: true,
@@ -68,20 +68,20 @@ export const commands = [
 
 ];
 export const projects = [
-    { 'No.': 1, title: 'xP',                    description: 'A cross platform C++ native app to test HTTP endpoints, available for Windows, Linux and Mac ',   document_url: renderLink('https://github.com/theProgrammerDavid/xP',                'xP') },
-    { 'No.': 2, title: 'HasherDB',              description: 'My own implementation of an in-memory NoSQL Database written in C++',                             document_url: renderLink('https://github.com/theProgrammerDavid/MyDB',              'HasherDB') },
-    { 'No.': 3, title: 'ThreeJs Playground',    description: 'A 3D playground for cool ThreeJs stuff',                                                          document_url: renderLink('https://theprogrammerdavid.github.io/three-js-samples',   'Playground') },
-    { 'No.': 4, title: 'Portfolio Website',     description: 'My own portfolio website built with Jquery and Snowpack',                                         document_url: renderLink('https://github.com/theProgrammerDavid/portfolio',         'Portfolio') },
-    { 'No.': 5, title: 'CP and Cheat Sheets',   description: 'Collection of Competitive programming practice questions and language cheat sheet',               document_url: renderLink('https://cheatsheet.davidvelho.tech/',                     'Cheat Sheets') },
+    { 'No.': 1, title: 'xP', description: 'A cross platform C++ native app to test HTTP endpoints, available for Windows, Linux and Mac ', document_url: renderLink('https://github.com/theProgrammerDavid/xP', 'xP') },
+    { 'No.': 2, title: 'HasherDB', description: 'My own implementation of an in-memory NoSQL Database written in C++', document_url: renderLink('https://github.com/theProgrammerDavid/MyDB', 'HasherDB') },
+    { 'No.': 3, title: 'ThreeJs Playground', description: 'A 3D playground for cool ThreeJs stuff', document_url: renderLink('https://theprogrammerdavid.github.io/three-js-samples', 'Playground') },
+    { 'No.': 4, title: 'Portfolio Website', description: 'My own portfolio website built with Jquery and Snowpack', document_url: renderLink('https://github.com/theProgrammerDavid/portfolio', 'Portfolio') },
+    { 'No.': 5, title: 'CP and Cheat Sheets', description: 'Collection of Competitive programming practice questions and language cheat sheet', document_url: renderLink('https://cheatsheet.davidvelho.tech/', 'Cheat Sheets') },
 
 ];
 export const achievements = [
-    { 'No.': 1, title: 'LoR Dean SCOPE VIT, TVS Pilot Project', document_url: renderLink('https://raw.githubusercontent.com/theProgrammerDavid/portfolio/master/static/tvs_cert.jpeg',      'TVS') },
-    { 'No.': 2, title: '2nd place Bolt Hack',                   document_url: renderLink('https://raw.githubusercontent.com/theProgrammerDavid/portfolio/master/static/bolt_hack.jpeg',     'Bolt Hack') },
-    { 'No.': 3, title: 'Freelance - OMR Corrector',             document_url: renderLink('https://raw.githubusercontent.com/theProgrammerDavid/portfolio/master/static/mahss_omr.jpeg',     'Freelance-Mahss') },
-    { 'No.': 4, title: '2nd place Cisco CTF',                   document_url: renderLink('https://raw.githubusercontent.com/theProgrammerDavid/portfolio/master/static/ciscoCTF.png',       'Cisco CTF') },
-    { 'No.': 5, title: 'Samsung PRISM Ambient Temp Detection',  document_url: renderLink('https://raw.githubusercontent.com/theProgrammerDavid/portfolio/master/static/OD25VIT_David_.pdf', 'Samsung PRISM') },
-    { 'No.': 6, title: 'Camcann Systems Programmer Intern',     document_url: renderLink('https://raw.githubusercontent.com/theProgrammerDavid/portfolio/master/static/camcann.jpg',        'CamCann SDE Intern') }
+    { 'No.': 1, title: 'LoR Dean SCOPE VIT, TVS Pilot Project', document_url: renderLink('https://raw.githubusercontent.com/theProgrammerDavid/portfolio/master/static/tvs_cert.jpeg', 'TVS') },
+    { 'No.': 2, title: '2nd place Bolt Hack', document_url: renderLink('https://raw.githubusercontent.com/theProgrammerDavid/portfolio/master/static/bolt_hack.jpeg', 'Bolt Hack') },
+    { 'No.': 3, title: 'Freelance - OMR Corrector', document_url: renderLink('https://raw.githubusercontent.com/theProgrammerDavid/portfolio/master/static/mahss_omr.jpeg', 'Freelance-Mahss') },
+    { 'No.': 4, title: '2nd place Cisco CTF', document_url: renderLink('https://raw.githubusercontent.com/theProgrammerDavid/portfolio/master/static/ciscoCTF.png', 'Cisco CTF') },
+    { 'No.': 5, title: 'Samsung PRISM Ambient Temp Detection', document_url: renderLink('https://raw.githubusercontent.com/theProgrammerDavid/portfolio/master/static/OD25VIT_David_.pdf', 'Samsung PRISM') },
+    { 'No.': 6, title: 'Camcann Systems Programmer Intern', document_url: renderLink('https://raw.githubusercontent.com/theProgrammerDavid/portfolio/master/static/camcann.jpg', 'CamCann SDE Intern') }
 
 ];
 export const socials = [
@@ -89,7 +89,8 @@ export const socials = [
     { 'No': 2, website: 'StackOverflow', url: 'https://stackoverflow.com/users/14312583/goanmafia' },
     { 'No': 3, website: 'Artstation', url: 'https://www.artstation.com/david_velho' },
     { 'No': 4, website: '500px', url: 'https://500px.com/p/david_velho?view=photos' },
-    { 'No': 5, website: 'Linkedin', url: 'https://www.linkedin.com/in/david-velho-aba95a173/' }
+    { 'No': 5, website: 'Linkedin', url: 'https://www.linkedin.com/in/david-velho-aba95a173/' },
+    { 'No': 6, website: 'dev.to', url: 'https://dev.to/theprogrammerdavid' }
 ]
 export const myPic = () => { return get_image('https://avatars1.githubusercontent.com/u/35698009?s=460&u=988e5ad85edb20cf16aaeeb8ea3e8b44088a582c&v=4') }
 
